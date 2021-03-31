846faf0128e6cba03732b669be06ee3b13a2e6da
Jeferson Ryan <jefersonryan16@gmail.com>
2021-03-31 17:53:03 -0300

    Adicionando HtmlParser

--- Classes/Table.js
+++ Classes/Table.js
@@ -4,7 +4,6 @@ class Table {
         arr.shift();
         this.rows = arr;
     }
-
     get  RowCount(){
         return this.rows.length;
     }
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const Reader = require("./Classes/Reader");
 const Processor = require('./Classes/Processor');
 const Table = require('./Classes/Table');
+const HtmlParser =  require('./Classes/HtmlParser');
 
 const leitor = new Reader();
-
 
 async function main(){
     let dados = await leitor.Read("./USERS.CSV");
     let processDados = Processor.Process(dados);
     let users = new Table(processDados);
+    let html = await HtmlParser.parser(users);
 }
 
 main();
