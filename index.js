5fbbe0924ef2ecb23e7cfee56b377b551322f325
Jeferson Ryan <jefersonryan16@gmail.com>
2021-03-31 18:00:03 -0300

    Gerando escritor me HTML

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const Reader = require("./Classes/Reader");
 const Processor = require('./Classes/Processor');
 const Table = require('./Classes/Table');
 const HtmlParser =  require('./Classes/HtmlParser');
+const Writer = require("./Classes/Writer");
 
 const leitor = new Reader();
+const escritor = new Writer();
 
 async function main(){
     let dados = await leitor.Read("./USERS.CSV");
     let processDados = Processor.Process(dados);
     let users = new Table(processDados);
     let html = await HtmlParser.parser(users);
+    escritor.Write("meuhtmlgerado.html",html);
 }
 
 main();
